0a780674351359cf47b7661cc518bc51f77a2b93
Sergei Brykalov <106953297+Sebryk@users.noreply.github.com>
2023-06-27 22:56:00 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="calculator">
+        <div className="calculator__screen">12345,54645</div>
+        <button className="span-two">AC</button>
+        <button>Del</button>
+        <button>&#247;</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>&#215;</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>&#8722;</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>&#43;</button>
+        <button>0</button>
+        <button>,</button>
+        <button className="span-two">&#61;</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
